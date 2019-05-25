743c7581f2d6841fbcf78967dbca89cb4d5d19e1
joshua <mama27j@gmail.com>
2019-05-25 12:01:44 +0300

    create new header component

---
+++ src/app/header-todo/header-todo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-todo',
+  templateUrl: './header-todo.component.html',
+  styleUrls: ['./header-todo.component.css']
+})
+export class HeaderTodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
